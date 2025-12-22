766d36f84de3f4ea851d838ddb30e433df83d23a
whysmx <185018402@qq.com>
2025-12-22 18:40:58 +0800

    Log command name mapping and force UpdateCommandUI

--- PublishExtension/PublishExtensionPackage.cs
+++ PublishExtension/PublishExtensionPackage.cs
@@ -31,6 +31,19 @@ namespace PublishExtension
                 var paneGuid = new Guid("9C47EA07-7688-4A7C-B2C8-AD5B5B1B2521");
                 outputWindow.CreatePane(ref paneGuid, "Eigcac发布", 1, 1);
             }
+            var uiShell = await GetServiceAsync(typeof(SVsUIShell)) as IVsUIShell;
+            if (uiShell != null)
+            {
+                uiShell.UpdateCommandUI(1);
+                ActivityLog.LogInformation("PublishExtension", "已调用 UpdateCommandUI。");
+            }
+            var cmdNameMapping = await GetServiceAsync(typeof(SVsCmdNameMapping)) as IVsCmdNameMapping;
+            if (cmdNameMapping != null)
+            {
+                LogCommandName(cmdNameMapping, PublishCommand.CommandSet, PublishCommand.CommandId);
+                LogCommandName(cmdNameMapping, PublishCommand.CommandSet, PublishCommand.CommandProjectId);
+                LogCommandName(cmdNameMapping, PublishCommand.CommandSet, PublishCommand.CommandSolutionId);
+            }
             try
             {
                 ActivityLog.LogInformation("PublishExtension", "包已初始化，准备加载菜单与命令。");
@@ -50,6 +63,13 @@ namespace PublishExtension
             {
                 // Ignore logging errors.
             }
+        }
+
+        private static void LogCommandName(IVsCmdNameMapping mapping, Guid guid, int id)
+        {
+            var cmdGuid = guid;
+            var hr = mapping.MapGUIDIDToName(ref cmdGuid, (uint)id, VSCMDNAMEOPTS.VSCMDNAMEOPTS_Default, out var name);
+            ActivityLog.LogInformation("PublishExtension", $"命令映射: {guid} {id} hr=0x{hr:X8} name={name ?? "<null>"}");
         }
     }
 }
